33614108a1bb95f818032fad3ef1e94080336650
kosyin <115675494+kosyin@users.noreply.github.com>
2024-05-10 21:53:28 -0400

    Add files via upload
    Cleaned up displayed P-State preferences and added end-user visual clarity.

--- main.py
+++ main.py
@@ -3,10 +3,17 @@
 # Imports "os" to run bash scripts.
 import os
 
-# Inputs can be "default," "performance," "balance_performance," "balance_power," or "power." Self explanatory.
-preference = input("Enter the preferred option for AMD's P-State: '1' for 'power,' '2' for 'balance_power,' '3' for 'balance_performance,' '4' for 'performance,' and '5' for 'default.'\n")
+# Inputs can be "power," "balance_power," "balance_performance," "performance," or "default." Self explanatory.
+print("Input the number value for the preferred AMD P-State power preference:")
+print("1. power")
+print("2. balance_power")
+print("3. balance_performance")
+print("4. performance")
+print("5. default")
 
-# Determine output.
+preference = input("Input: ")
+
+# Determine output and P-State setting.
 if preference == "1":
     os.system("echo 'AMD P-State power preference set to:' $(echo 'power' | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference)")
 elif preference == "2":
@@ -18,5 +25,5 @@ elif preference == "4":
 elif preference == "5":
         os.system("echo 'AMD P-State power preference set to:' $(echo 'default' | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference)")
 else:
-    print("An unrecognized input has been made. Aborting.") # In the event of a non-answer.
+    print("An unrecognized input has been made: aborting.") # In the event of a non-answer.
 
